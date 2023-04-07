c0e94c5e5f2b5db52c3c9bf855c697ec0869a3ed
Shunji Zhan <shunhahaha@live.com>
2023-04-07 12:45:25 +0800

    add params validation middleware

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { string, object, ObjectSchema, ValidationError } from 'yup';
+import { RouteArgsXcm } from '../route';
+
+const routeArgsXcmSchema: ObjectSchema<RouteArgsXcm> = object({
+  routerChainId: string().required(),
+  originAddr: string().required(),
+  targetChain: string().required(),
+  dest: string().required(),
+});
+
+export const validateRouteXcmArgs = () => {
+  return async (req: Request<any, any, any, RouteArgsXcm>, res: Response, next: NextFunction) => {
+    try {
+      await routeArgsXcmSchema.validate(req.query, { abortEarly: false });
+      next();
+    } catch (err) {
+      return res.status(200).json({
+        msg: err.errors,
+        shouldRoute: false,
+      });
+    }
+  };
+};
