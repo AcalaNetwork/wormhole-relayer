2d0af71b024b3be018aebc2cca9bf5699e0860ca
Shunji Zhan <shunhahaha@live.com>
2023-07-18 12:59:35 +0800

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
       reportsDirectory: './coverage',
-      enabled: true,
+      enabled: false,
       exclude: ['**/__tests__'],
     },
   },
