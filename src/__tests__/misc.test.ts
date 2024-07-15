8b8cffb110a2777742fb40944ccb52d3aada3ae6
Shunji Zhan <shunhahaha@live.com>
2024-07-15 17:15:35 +0800

    wip

--- src/__tests__/misc.test.ts
+++ src/__tests__/misc.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { expectError, health, noRoute, testTimeout, version } from './testUtils';
+import { expectError, health, noRoute, routeHoma, shouldRouteHoma, testTimeout, version } from './testUtils';
 
 describe.concurrent('/miscellaneous', () => {
   describe('when no route for the request', () => {
@@ -40,6 +40,29 @@ describe.concurrent('/miscellaneous', () => {
       expect(Number(res.data.relayerBalAcala)).to.be.gt(0);
       // expect(Number(res.data.relayerBalKarura)).to.be.gt(0);
       // expect(res.data.isHealthy).to.be.true;
+    });
+  });
+
+  describe.only('/multiple simaltaneous shouldRoute', () => {
+    it('works', async () => {
+      const routeParams = {
+        destAddr: '0x75E480dB528101a381Ce68544611C169Ad7EB342',
+        chain: 'acala',
+      };
+
+      const routePending = routeHoma(routeParams);
+
+      const pendings = [] as any[];
+      for (let i = 0; i < 10; i++) {
+        pendings.push(shouldRouteHoma(routeParams));
+      }
+
+      const res = await Promise.all(pendings);
+
+      const routeRes = await routePending;
+      console.log(routeRes)
+
+      console.log(res);
     });
   });
 });
