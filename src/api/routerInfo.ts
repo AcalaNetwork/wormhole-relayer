d6acc3a357a076200779256e30431aebe932c403
Shunji Zhan <shunhahaha@live.com>
2024-09-10 14:20:05 +0800

    update

--- src/api/routerInfo.ts
+++ src/api/routerInfo.ts
@@ -1,8 +1,4 @@
 import { RouterInfo, db } from '../db';
-import {
-  _populateRelayTx,
-  _populateRouteTx,
-} from '../utils';
 
 export const getRouterInfo = async (params: Partial<RouterInfo>) => {
   const records = await db.getRouterInfo(params);
